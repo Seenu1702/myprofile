c1f435cfad3f95a135a35a4d8867594c8b6a6eb7
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-10-14 16:52:10 +0530

    default code simplification

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 const App = () =>{
+  //our first react component
+  // also javascript funtion
+  // for now it does not receive any components
+
+  // it can also contain or execute javascript code
+  console.log("Hello from Component App")
+
   return(
+    
+    // returns a html
     <div>
       <p>Hello Seenu</p>
     </div>
