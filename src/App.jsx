d2a202539b2c4347af7f511b8e404e9cfe3f9cb1
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-10-15 12:18:23 +0530

    moving list item in Note component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 /* eslint-disable react/prop-types */
 
+function Note({note}) {
+  return(
+    <li> { note.content } </li>
+  )
+}
+
 function App(props) {
-  // console.log(props);
-
-  // destructing
   const {notes} = props;
-  // console.log(notes);
-
   return (
     <div>
       <h1>Notes</h1>
-      
+
       <ul>
         {
           notes.map((note) =>{
-            return <li key={ note.id }> { note.content } </li>
+            return <Note key={ note.id } note = {note} />
           })
         }
       </ul>
-
-      <select name="" id="">
-        {
-          notes.map((note) =>{
-            return <option key={ note.id }> { note.content } </option>
-          })
-        }
-      </select>
     </div>
   )
 }
