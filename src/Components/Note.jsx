5a19e0f47e78e22e959cfcd5d393507ed1f150e9
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-10-15 12:58:58 +0530

    conditional rendering uusing if

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable react/prop-types */
 function Note({note}) {
+  if(note.important) {
     return(
-      <li> { note.content } </li>
+      <li> ✅ { note.content } </li>
     )
+  }
+  return(
+    <li> ❌ { note.content } </li>
+  )
   }
 
 export default Note
