43de777a238e8eca92db79effc758eb1bfa6b476
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-10-16 12:20:39 +0530

    passing props in functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-// import React from 'react' // not necessary from React 17
-// Function componenent
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react'
 
-
-
-// function App() {
-//   return (
-//     <div>Hello World!</div>
-//   )
-// }
-
-// export default App
-
-// class componenent
-import { Component } from "react";
-
-class App extends Component {
-  render() {
-    return(
-      <div>Hello World!</div>
-    )
-  }
+//passing props in functional component
+function Hello({ name }){
+  return(
+    <div>Hello { name }!</div>
+  )
 }
 
-export default App;
+function App() {
+  return (
+    <Hello name="Seenu"/>
+  )
+}
+
+export default App
