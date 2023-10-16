3d1c4d0de4e84c826de3ca3cacc4584b332ec3a2
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-10-16 21:56:51 +0530

    class component life cycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-/* eslint-disable react/prop-types */
-import { Component } from "react";
-
-class Hello extends Component{
-  render() {
-
-    // console.log(this.props);
-    const {name} = this.props;
-
-    return(
-      // <div>Hello {this.props.name}!</div>
-      <div>Hello {name}!</div>
-
-    )
-  }
-}
+import { Component } from 'react'
 
 class App extends Component{
-  render() {
+
+  //called after the component has to be rendered to the DOM
+  componentDidMount(){
+    console.log('Component Mounted')
+  }
+
+  //called immediately after the component updated in the DOM
+  componentDidUpdate(){
+    console.log('Component state updated')
+  }
+
+  componentWillUnmount(){
+    //invoked just before the component is removed from DOM
+    //releasing memory or resource    
+    //detaching the events
+    //canceling the timer
+  }
+
+
+  render(){
     return(
-      <div>
-        <Hello name='Seenu' />
-      </div>
+      <div>Hello World!</div>
     )
   }
 }
