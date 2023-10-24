9aa7852d4b1b351b20bfda6ce6cbe427f7c45216
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-10-24 16:44:18 +0530

    added bootstrap with npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
-import { Component } from "react";
+import React from 'react';
+import './assets/App.css';
 
-// stateful Component
-class App extends Component {
-  constructor(props){
-    super(props); //to call the parent class constructor  
+function App() {
+  return (
+    <div className='mt-4 p-2' >
+      <h1>Hello World!</h1>
 
-    // defining the state
-    this.state = {
-      count: 0
-    }
-  }
-
-  handleIncrementClick = () => {
-    // update the state(data) of the count
-    this.setState((prevState) => ({
-      count: prevState.count + 1
-    }))
-  }
-
-  handleDecrementClick = () => {
-    if(this.state.count > 0)
-    {
-      this.setState((prevState) => ({
-        count: prevState.count - 1
-      }))
-    }
-  }
-
-  handleResetClick = () => {
-    this.setState((prevState) => ({
-      count: 0
-    }))
-  }
-
-  render(){
-    return(
-      <div>
-        <p>Count: { this.state.count }</p> 
-        <button onClick={ this.handleIncrementClick }>Increment</button>
-        <button onClick={ this.handleDecrementClick }>Decrement</button>
-        <button onClick={ this.handleResetClick }>Reset</button>
-        
-      </div>
-    )
-  }
+      <p className='para'>This is a paragraph in red color</p>
+    </div>
+    
+  )
 }
 
-export default App
+export default App;
