71e340eeeacec36fa89eb3972958057834b5aae1
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-10-31 12:03:48 +0530

    draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
-import './assets/App.css';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable react/no-direct-mutation-state */
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className='mt-4 p-2' >
-      <h1>Hello World!</h1>
+class App extends Component{
 
-      <p className='para'>This is a paragraph in red color</p>
-    </div>
+  constructor(props){
+    super(props);
     
-  )
+    this.state = {
+      goodCount : 0,
+      neutralCount : 0,
+      badCount : 0
+    }
+  }
+
+   handleGood = () =>{
+    this.setState({
+      count: this.state.goodCount
+      ,
+    }) 
+  }
+
+   handleNeutral = () =>{
+
+  }
+
+   handleBad = () =>{
+
+  }
+
+  render() {
+    return(
+      
+      <><div>
+        <h1>Give Feedback</h1>
+        <button onClick={this.handleGood}>Good</button>
+        <button onClick={this.handleNeutral}>Neutral</button>
+        <button onClick={this.handleBad}>Bad</button>
+      </div>
+      <div>
+        <p>Statistics:</p> 
+        <p>Good: {this.state.goodCount} </p>
+        <p>Neutral: </p>
+        <p>Bad: </p>
+      </div>
+      </>
+        
+
+      
+    )
+  }
 }
 
 export default App;
