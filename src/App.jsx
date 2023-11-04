90dd8428421283dd795ade8043881c9b38b31358
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-11-04 20:25:36 +0530

    useEffect hook with dependency

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,22 @@
 import React, { useEffect, useState } from 'react'
 
 function App() {
-  // create a state to store the data fetched from the API
+  const [count,setCount] = useState(0);
 
-  const [data, setData] = useState(null);
+  useEffect(() => {
+    document.title =` Count: ${count}`
 
-  //use the useEffect hook to run the function to call the API only one time
+  },[count]);
 
-  useEffect(()=>{
-    fetch(`https://jsonplaceholder.typicode.com/posts`)
-      .then(response => response.json())
-      .then(result => setData(result))
+    const handleClick = () => {
+      setCount(count + 1);
+    }
 
-  },[]);
-
-  // console.log(data);
+    // console.log(count);
 
   return (
     <div>
-      <h1>API Data: </h1>
-      {
-        data ? (
-          <ul>
-            {
-               data.map(item => {
-                return <li key={item.id}>{item.title}</li>
-               })
-            }
-          </ul>
-        ) : (
-          <p>Loading....</p>
-        )
-      }
+      <button onClick={handleClick}>Increment Change</button>
     </div>
   )
 }
