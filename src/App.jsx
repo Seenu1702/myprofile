6d120c18f3a5a8bb0fd766bdb9f940cac5a4ea50
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-11-06 22:44:38 +0530

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,30 @@ function App(props) {
   // console.log(props.notes);
 
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+  // console.log(notes);
+  const addNote = (event) => {
+    event.preventDefault();
+    // console.log('button clicked...');
+    // console.log(newNote);
+    const newObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
 
-  console.log(notes);
+    setNotes(notes.concat(newObject));
+    setNewNote('');
+
+
+  }
+
+  const handleNoteChange = (event) =>{
+    // console.log(event.target.value);
+    setNewNote(event.target.value);
+    // console.log(newNote)
+  }
+
   return (
     <div>
       <h1>Notes App</h1>
@@ -18,6 +40,14 @@ function App(props) {
             )
         }
       </ul>
+
+      <form onSubmit={addNote} >
+        <input 
+        value={newNote}
+        placeholder='a new note....'
+        onChange={handleNoteChange}/>
+        <button type='submit'>Save Note</button>
+      </form>
      
       
     </div>
