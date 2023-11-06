a5d116e8217e7e152e62b7367c72e0dcc20284b4
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-11-06 11:58:46 +0530

    props drilling - parent to child to grandchild

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,15 @@
-/* eslint-disable no-unused-vars */
-// import React from 'react'
-import { useState } from 'react';
-import Home from './Components/Home';
-import Users from './Components/Users';
-import Notes from './Components/Notes';
+// import React from 'react';
+import ChildComponent from './Components/ChildComponent';
+
 
 function App() {
-  const [page, setPage] = useState('home');
-
-  const toPage = (page) => (event) =>{
-    event.preventDefault();
-    setPage(page);
-  }
-
-  const content = () =>{
-    if(page === 'home'){
-      return <Home />
-    }
-    else if(page === 'users'){
-      return <Users />
-    }
-    else if(page === 'notes'){
-      return <Notes />
-    }
-  }
-
-  const padding = {
-    padding: 10
-  }
+  const message = 'Hello, Child...'
 
   return (
     <div>
-      <a href="" onClick={toPage('home')} style={padding}>Home</a>
-      <a href="" onClick={toPage('users')}style={padding}>Users</a>
-      <a href="" onClick={toPage('notes')}style={padding}>Notes</a>
-
-      {content()}
+      <h1>Parent Component</h1>
+      <ChildComponent message={message}/>
     </div>
-
-    
   )
 }
 
