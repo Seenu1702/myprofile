a558e70f0911aa9541ac299a0fdd3b6e64012309
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-11-13 18:00:40 +0530

    useReducer example

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useReducer } from 'react';
 import { useState } from 'react';
+import { initialState, reducer } from './Reducer/Count';
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count+1)}>Increment</button>
-      <button onClick={() => setCount(count-1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <p>Count: {state.count}</p>
+      <button onClick={() =>dispatch({type:'increment'})}>Increment</button>
+      <button onClick={() =>dispatch({type:'decrement'})}>Decrement</button>
+      <button onClick={() =>dispatch({type:'reset'})}>Reset</button>
     </div>
   )
 }
