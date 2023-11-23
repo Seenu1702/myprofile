053d489f48fbd5240e25efe1ea55001209080fda
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-11-23 15:32:07 +0530

    read operation  showing multiple notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,87 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import './Styles/App.css';
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
 
+function App(props) {
 
-function Button({value, onClick}) {
-  return (
-    <button className='square' onClick={onClick}>{ value }</button>
-  )
-}
+  // console.log(props.notes);
 
-function App() {
+  // define states
+  
+  const [notes, setNotes] = useState(props.notes);
+  const [showStatus, setShowStatus] = useState('all');
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+  // const [notes, setNotes] = useState([]);
+  // get the data
+  // useEffect(() =>{
+  //   setNotes(props.notes);
+  // }, []);
 
-  function handleClick(i){
-
-    if(squares[i] || calculateWinner(squares)){
-      return;
-    }
-
-    const nextSquares =  squares.slice();
-
-    if(xIsNext){
-      nextSquares[i] = 'X';
-    }
-    else{
-      nextSquares[i] = 'O';
-    }
-    
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-       
-  }
-
-  function calculateWinner(squares){
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for(let i=0;i<lines.length;i++){
-      const [a, b, c] = lines[i];
-
-      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  let winner = calculateWinner(squares);
-  let status;
-  if(winner)
-  {
-    status = 'Winner: ' + winner;
-  }
-  else 
-  {
-    status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
-  }
+ const handleStatusChange = (event) => {
+  
+  setShowStatus(event.target.value);
   
 
+ }
+
+ let filteredNotes = (notes, showStatus) => {
+  switch(showStatus){
+    case 'all':
+      return notes;
+    case 'imp':
+      return notes.filter((note) => note.important === true);
+    case 'nonimp':
+      return notes.filter((note) => note.important === false);
+  }
+
+ }
+
+ const notesFiltered = filteredNotes(notes, showStatus);
 
   return (
     <div>
-      <div>
-        {status} 
-      </div>
-      <div className='rows'>
-        <Button value={squares[0]} onClick={() => handleClick(0)}/>
-        <Button value={squares[1]} onClick={() => handleClick(1)}/>
-        <Button value={squares[2]} onClick={() => handleClick(2)}/>
-      </div>
-      <div className='rows'> 
-        <Button value={squares[3]} onClick={() => handleClick(3)}/>
-        <Button value={squares[4]} onClick={() => handleClick(4)}/>
-        <Button value={squares[5]} onClick={() => handleClick(5)}/>
-      </div>
-      <div className='rows'>
-        <Button value={squares[6]} onClick={() => handleClick(6)}/>
-        <Button value={squares[7]} onClick={() => handleClick(7)}/>
-        <Button value={squares[8]} onClick={() => handleClick(8)}/>
-      </div>     
+      <h1>Notes</h1>
+
+      <label>
+        <input 
+        type="radio"
+        name='filter'
+        checked={showStatus === 'all'}
+        onChange={handleStatusChange}
+        value='all' />
+        All Notes
+      </label>
+
+      <label>
+        <input 
+        type="radio"
+        name='filter'
+        checked={showStatus === 'imp'}
+        onChange={handleStatusChange}
+        value='imp' />
+        Important Notes
+      </label>
+
+      <label>
+        <input 
+        type="radio"
+        name='filter'
+        checked={showStatus === 'nonimp'}
+        onChange={handleStatusChange}
+        value='nonimp' />
+        Non-Important Notes
+      </label>
+
+      <ul>
+        {
+          notesFiltered.map((note) => {
+            return <li key={note.id}>{ note.content }</li>
+          })
+        }
+      </ul>
     </div>
   )
 }
 
-export {
-  App as default,
-  Button
-}
+export default App;
