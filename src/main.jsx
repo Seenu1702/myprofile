14e4fe30e3fc36f11bfa58be0fb82308535d0878
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-12-05 19:48:21 +0530

    redux state management

--- src/main.jsx
+++ src/main.jsx
@@ -2,34 +2,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 
-const notes = [
-    {
-        id: 1,
-        content: "Working with List",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Usage of Keys",
-        important: false
-    },
-    {
-        id: 3,
-        content: "Conditional Rendering",
-        important: true
-    },
-    {
-        id: 4,
-        content: "Handling Events",
-        important: true
-    },
-    {
-        id: 5,
-        content: "Prop vs State",
-        important: false
+const counterReducer = (state = 0, action) => {
+    switch(action.type){
+        case 'INCR':
+            return state + 1;
+        case 'DECR':
+            return state - 1;
+        case 'ZERO':
+            return 0;
+        default:
+            return state;
     }
-];
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const store = createStore(counterReducer);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
