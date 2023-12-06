e92603a3c192e887274eccf35fa61277e612340b
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-12-06 19:01:14 +0530

    view notes with redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,40 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
-
-const counterReducer = (state = 0, action) => {
-    switch(action.type){
-        case 'INCR':
-            return state + 1;
-        case 'DECR':
-            return state - 1;
-        case 'ZERO':
-            return 0;
-        default:
-            return state;
-    }
-}
-
-const store = createStore(counterReducer);
-
-
-// subscribe method is called whenever we dispatch an action
-store.subscribe(() => {
-    const stateNow = store.getState();
-    console.log(stateNow);
-})
-
-store.dispatch({type:'INCR'});
-store.dispatch({type:'INCR'});
-store.dispatch({type:'DECR'});
-store.dispatch({type:'INCR'});
-store.dispatch({type:'INCR'});
-
-
-console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <App />)
